ec19f18c69c7cac2519e06c26e05837d941da8ed
Jython1415 <36921818+Jython1415@users.noreply.github.com>
2021-04-15 17:20:26 +0900

    added to route and split semantically

--- team-c-competition/include/auton/routes.h
+++ team-c-competition/include/auton/routes.h
@@ -7,6 +7,21 @@ void chassis_reset();
 // pause in ms
 void pause(int input);
 
+// controlled forward/backward movement
+void f_c(int dist, int max_pwr);
+
+// controlled turn
+void t_c(int dist, int max_pwr);
+
+// pause and reset chassis encoders
+void p_r();
+
+// score the first tower and back up
+void route_p1 ();
+
+// score the 2nd tower and back up
+void route_p2 ();
+
 void routes () ;
 
 #endif
--- team-c-competition/src/auton/routes.cpp
+++ team-c-competition/src/auton/routes.cpp
@@ -23,8 +23,131 @@ void pause(int input)
   vex::task::sleep(input);
 }
 
-void routes ()
+// controlled forward/backward movement
+void f_c(int dist, int max_pwr)
 {
+  // forward movement
+  if (dist > 0)
+  {
+    // acc 1
+    chassis_set(int(max_pwr/3) * 1);
+
+    // acc 2
+    waitUntil(rot_LB > int(dist/6) * 1);
+    chassis_set(int(max_pwr/3) * 2);
+
+    // cruise
+    waitUntil(rot_LB > int(dist/6) * 2);
+    chassis_set(max_pwr);
+
+    // decc 1
+    waitUntil(rot_LB > int(dist/6) * 4);
+    chassis_set(int(max_pwr/3) * 2);
+
+    // decc 2
+    waitUntil(rot_LB > int(dist/6) * 5);
+    chassis_set(int(max_pwr/3) * 1);
+
+    // stop
+    waitUntil(rot_LB > dist);
+    chassis_stop();
+  }
+  // backward movement
+  else
+  {
+    // acc 1
+    chassis_set(-int(max_pwr/3) * 1);
+
+    // acc 2
+    waitUntil(rot_LB < int(dist/6) * 1);
+    chassis_set(-int(max_pwr/3) * 2);
+
+    // cruise
+    waitUntil(rot_LB < int(dist/6) * 2);
+    chassis_set(-max_pwr);
+
+    // decc 1
+    waitUntil(rot_LB < int(dist/6) * 4);
+    chassis_set(-int(max_pwr/3) * 2);
+
+    // decc 2
+    waitUntil(rot_LB < int(dist/6) * 5);
+    chassis_set(-int(max_pwr/3) * 1);
+
+    // stop
+    waitUntil(rot_LB < dist);
+    chassis_stop();
+  }
+}
+
+// controlled turn
+void t_c(int dist, int max_pwr)
+{
+  // forward movement
+  if (dist > 0)
+  {
+    // acc 1
+    chassis_set_turn(int(max_pwr/3) * 1);
+
+    // acc 2
+    waitUntil(rot_LB < int(dist/6) * 1);
+    chassis_set_turn(int(max_pwr/3) * 2);
+
+    // cruise
+    waitUntil(rot_LB < int(dist/6) * 2);
+    chassis_set_turn(max_pwr);
+
+    // decc 1
+    waitUntil(rot_LB < int(dist/6) * 4);
+    chassis_set_turn(int(max_pwr/3) * 2);
+
+    // decc 2
+    waitUntil(rot_LB < int(dist/6) * 5);
+    chassis_set_turn(int(max_pwr/3) * 1);
+
+    // stop
+    waitUntil(rot_LB < dist);
+    chassis_stop();
+  }
+  // backward movement
+  else
+  {
+    // acc 1
+    chassis_set_turn(-int(max_pwr/3) * 1);
+
+    // acc 2
+    waitUntil(rot_LB > int(dist/6) * 1);
+    chassis_set_turn(-int(max_pwr/3) * 2);
+
+    // cruise
+    waitUntil(rot_LB > int(dist/6) * 2);
+    chassis_set_turn(-max_pwr);
+
+    // decc 1
+    waitUntil(rot_LB > int(dist/6) * 4);
+    chassis_set_turn(-int(max_pwr/3) * 2);
+
+    // decc 2
+    waitUntil(rot_LB > int(dist/6) * 5);
+    chassis_set_turn(-int(max_pwr/3) * 1);
+
+    // stop
+    waitUntil(rot_LB > dist);
+    chassis_stop();
+  }
+}
+
+// pause and reset chassis encoders
+void p_r()
+{
+  pause(500);
+  chassis_reset();
+}
+
+// score the 1st tower and back up
+void route_p1 ()
+{
+  // score preload
   rollerB_set(100);
   waitUntil(rollerB.rotation(vex::rotationUnits::deg) > 180);
   rollerB_set(0);
@@ -84,11 +207,11 @@ void routes ()
   // stop outaking
   roller_set(0);
   intake_set(0);
+}
 
-  // reset
-  pause(500);
-  chassis_reset();
-
+// score the 2nd tower and back up
+void route_p2 ()
+{
   // turn right
   chassisL_set(5);
   chassisR_set(-30);
@@ -102,19 +225,77 @@ void routes ()
   chassis_reset();
 
   // forward
-  chassis_set(20);
-  waitUntil(rot_LB > 500);
-  chassis_set(40);
-  waitUntil(rot_LB > 1500);
+  f_c(1500, 60);
+
+  p_r(); // reset
+  
   // intake
   intake_set(100);
-  waitUntil(rot_LB > 2500);
+  waitUntil(rot_LB > 500);
   chassis_set(20);
-  waitUntil(rot_LB > 3000);
+  waitUntil(rot_LB > 1000);
   // stop
   intake_set(10);
   chassis_stop();
 
+  p_r(); // reset
+
   // turn left
-  // chassisL_set()
+  t_c(1000, 50);
+
+  // forward to goal
+  chassis_set(40);
+  pause(1000);
+  chassis_stop();
+
+  p_r(); // reset
+
+  // shoot (time)
+  intake_set(100);
+  roller_set(100);
+  pause(600);
+  // remove blue balls
+  rollerT_set(-5);
+  rollerB_set(60);
+  pause(250);
+  // stop (intake out to avoid descore)
+  intake_set(-20);
+  roller_set(0);
+  // reset encoders
+  chassis_reset();
+  // back away
+  chassis_set(-20);
+  waitUntil(rot_LB < -400);
+  roller_set(-100);
+  intake_set(-100);
+  f_c(-1000, 60);
+  // outake a bit more
+  pause(500);
+  // stop outaking
+  roller_set(0);
+  intake_set(0);
+}
+
+void routes ()
+{
+  route_p1();
+
+  p_r(); // reset
+
+  route_p2();
+
+  p_r(); // reset
+
+  // turn right
+  t_c(-1000, 50);
+
+  p_r(); // reset
+
+  // forward
+  f_c(2500, 60);
+
+  p_r(); // reset
+
+  // forward and intake
+
 }
